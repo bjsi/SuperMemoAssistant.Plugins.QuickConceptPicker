652bf4c2e5577b975532990a3b5c2ba05afdc442
james <james@DESKTOP-JDJA6NK>
2020-06-07 02:31:49 +0100

    Initial Commit, not working due to concepts issue in SMA

---
+++ src/SuperMemoAssistant.Plugins.QuickConceptPicker/QuickConceptPickerPlugin.cs
@@ -0,0 +1,102 @@
+﻿#region License & Metadata
+
+// The MIT License (MIT)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the 
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// 
+// 
+// Created On:   6/7/2020 2:09:22 AM
+// Modified By:  james
+
+#endregion
+
+
+
+
+namespace SuperMemoAssistant.Plugins.QuickConceptPicker
+{
+    using System.Diagnostics.CodeAnalysis;
+  using System.Windows;
+  using System.Windows.Input;
+  using SuperMemoAssistant.Extensions;
+  using SuperMemoAssistant.Plugins.QuickConceptPicker.UI;
+  using SuperMemoAssistant.Services;
+  using SuperMemoAssistant.Services.IO.Keyboard;
+  using SuperMemoAssistant.Services.Sentry;
+  using SuperMemoAssistant.Sys.IO.Devices;
+
+  // ReSharper disable once UnusedMember.Global
+  // ReSharper disable once ClassNeverInstantiated.Global
+  [SuppressMessage("Microsoft.Naming", "CA1724:TypeNamesShouldNotMatchNamespaces")]
+  public class QuickConceptPickerPlugin : SentrySMAPluginBase<QuickConceptPickerPlugin>
+  {
+    #region Constructors
+
+    /// <inheritdoc />
+    public QuickConceptPickerPlugin() : base("Enter your Sentry.io api key (strongly recommended)") { }
+
+    #endregion
+
+
+
+
+    #region Properties Impl - Public
+
+    /// <inheritdoc />
+    public override string Name => "QuickConceptPicker";
+
+    /// <inheritdoc />
+    public override bool HasSettings => false;
+
+    #endregion
+
+
+
+
+    #region Methods Impl
+
+    /// <inheritdoc />
+    protected override void PluginInit()
+    {
+      Svc.HotKeyManager
+        .RegisterGlobal(
+        "SelectConcept",
+        "Open Concept Selection Window",
+        HotKeyScopes.SMBrowser,
+        new HotKey(Key.O, KeyModifiers.Alt),
+        OpenConceptSelectionWdw
+      );
+    }
+    private void OpenConceptSelectionWdw()
+    {
+      Application.Current.Dispatcher.Invoke(() =>
+      {
+        var wdw = new ConceptPickerWdw();
+        wdw.ShowAndActivate();
+      });
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    #endregion
+  }
+}
---
+++ src/SuperMemoAssistant.Plugins.QuickConceptPicker/UI/ConceptPickerWdw.xaml.cs
@@ -0,0 +1,110 @@
+﻿using SuperMemoAssistant.Interop.SuperMemo.Registry.Members;
+using SuperMemoAssistant.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SuperMemoAssistant.Plugins.QuickConceptPicker.UI
+{
+  /// <summary>
+  /// Interaction logic for ConceptPickerWdw.xaml
+  /// </summary>
+  public partial class ConceptPickerWdw : Window
+  {
+    public ConceptPickerWdw()
+    {
+      var Concepts = Svc.SM.Registry.Concept.ToList();
+      if (Concepts == null || Concepts.Count == 0)
+      {
+        return;
+      }
+
+      InitializeComponent();
+      DataContext = this;
+
+      // TODO: Fails, I think due to accessing the Name field
+      Dictionary<string, IConcept> conceptMap = new Dictionary<string, IConcept>();
+      foreach (var concept in Concepts)
+      {
+        if (!string.IsNullOrEmpty(concept.Name))
+        {
+          conceptMap.Add(concept.Name, concept);
+        }
+      }
+
+      ConceptBox.DisplayMemberPath = "Key";
+      ConceptBox.SelectedValuePath = "Value";
+
+      ConceptBox.ItemsSource = conceptMap;
+
+      ConceptBox.Dispatcher.BeginInvoke((Action)(() =>
+      {
+        ConceptBox.Focus();
+      }), DispatcherPriority.Render);
+    }
+
+    private void ConceptBox_TextInput(object sender, TextChangedEventArgs e)
+    {
+      SelectedConceptTextblock.Content = string.Empty;
+
+      if (ConceptBox.SelectedItem != null)
+      {
+        var currentConcept = (IConcept)ConceptBox.SelectedValue;
+        SelectedConceptTextblock.Content = currentConcept.Name;
+      }
+    }
+
+    private void BtnCancel_Click(object sender,
+                             RoutedEventArgs e)
+    {
+      Close();
+    }
+
+    private void SetCurrentConcept()
+    {
+      if (ConceptBox.SelectedItem != null)
+      {
+        IConcept currentConcept = (IConcept)ConceptBox.SelectedValue;
+        Svc.SM.UI.ElementWdw.SetCurrentConcept(currentConcept.Id);
+      }
+    }
+
+    private void ReturnToElement(int elementId)
+    {
+      if (elementId < 0)
+      {
+        return;
+      }
+      Svc.SM.UI.ElementWdw.GoToElement(elementId);
+    }
+    private void BtnOk_Click(object sender,
+                         RoutedEventArgs e)
+    {
+
+      int returnElementId = Svc.SM.UI.ElementWdw.CurrentElementId;
+      SetCurrentConcept();
+      ReturnToElement(returnElementId);
+
+      Close();
+    }
+
+    private void ConceptBox_KeyDown(object sender, KeyEventArgs e)
+    {
+      if (e.Key == Key.Enter)
+      {
+        BtnOk_Click(sender, null);
+      }
+    }
+  }
+}
